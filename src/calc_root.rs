b3ab7bc9b02d934a68127f428b844f91035952f5
Luan Almeida <luan.almeida@jbtec.com.br>
2025-04-23 20:01:05 -0300

    feat: handle complex roots

--- src/calc_root.rs
+++ src/calc_root.rs
@@ -5,6 +5,8 @@ pub fn main(terms: [f64; 3]) -> (f64, f64) {
 
     let delta: f64 = term_b * term_b - 4.0 * term_a * term_c;
     if delta < 0.0 {
+        let complex_roots: String = complex_roots(delta, term_a, term_b);
+        println!("Raízes complexas: {}", complex_roots);
         return (0.0, 0.0);
     }
 
@@ -12,4 +14,10 @@ pub fn main(terms: [f64; 3]) -> (f64, f64) {
     let root_two = (-term_b - delta.sqrt()) / (2.0 * term_a);
 
     return (root_one, root_two);
+}
+
+fn complex_roots(delta: f64, a: f64, b: f64) -> String {
+    let real = format!("{}/{}", -b, (2.0 * a));
+    let imag = format!("√{}i/{}", -delta, (2.0 * a));
+    format!("{} +- {}", real, imag)
 }
--- src/calc_vertex.rs
+++ src/calc_vertex.rs
@@ -5,6 +5,5 @@ pub fn main(terms: [f64; 3]) -> (f64, f64) {
 
     let vertex_x: f64 = -b / (2.0 * a);
     let vertex_y: f64 = (a * (vertex_x * vertex_x)) + (b * vertex_x) + c;
-    println!("vertex_x: {}, vertex_y: {}", vertex_x, vertex_y);
     return (vertex_x, vertex_y);
 }
