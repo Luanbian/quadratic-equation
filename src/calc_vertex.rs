baffbbcd03e88923700d5887ad0941449233b111
Luan Almeida <luan.almeida@jbtec.com.br>
2025-04-23 19:21:19 -0300

    fix: vertex count

--- src/calc_vertex.rs
+++ src/calc_vertex.rs
@@ -3,7 +3,8 @@ pub fn main(terms: [f64; 3]) -> (f64, f64) {
     let b = terms[1];
     let c = terms[2];
 
-    let vertex_x: f64 = -b / 2.0 * a;
-    let vertex_y: f64 = vertex_x * vertex_x + b * vertex_x + c;
+    let vertex_x: f64 = -b / (2.0 * a);
+    let vertex_y: f64 = (a * (vertex_x * vertex_x)) + (b * vertex_x) + c;
+    println!("vertex_x: {}, vertex_y: {}", vertex_x, vertex_y);
     return (vertex_x, vertex_y);
 }
